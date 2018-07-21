e012720a7697cb02d8817d46b40bdea53655b751
Wolfgang Puffitsch <hausen@gmx.at>
2018-07-21 18:09:00 +0200

    Enable parallel workers in iterative solving algorithm.

--- src/csolve.c
+++ src/csolve.c
@@ -34,6 +34,7 @@ along with CSolve.  If not, see <http://www.gnu.org/licenses/>.
 // parallelizing data
 static int32_t _workers_max;
 static int32_t _worker_id;
+static int32_t _worker_min_depth;
 static struct shared_t *_shared;
 
 // restarting data
@@ -111,93 +112,78 @@ void shared_init(int32_t workers_max) {
   shared()->workers = 1;
   shared()->workers_id = 1;
   _worker_id = 1;
+  _worker_min_depth = 0;
 }
 
 struct shared_t *shared(void) {
   return _shared;
 }
 
-/* pid_t worker_spawn(struct env_t *env, size_t depth) { */
+void worker_spawn(struct env_t *env, size_t depth) {
 
-/*   if (env[depth+1].key == NULL) { */
-/*     // do not fork at last level */
-/*     return -1; */
-/*   } */
+  if (!is_value(*env[depth].val) && shared()->workers < _workers_max) {
 
-/*   if (!is_value(*env[depth].val) && shared()->workers < _workers_max) { */
+    sema_wait(&shared()->semaphore);
+    // avoid race condition
+    if (shared()->workers >= _workers_max) {
+      sema_post(&shared()->semaphore);
+      return;
+    }
+    ++shared()->workers;
+    int32_t id = ++shared()->workers_id;
+    sema_post(&shared()->semaphore);
 
-/*     sema_wait(&shared()->semaphore); */
-/*     // avoid race condition */
-/*     if (shared()->workers >= _workers_max) { */
-/*       sema_post(&shared()->semaphore); */
-/*       return -1; */
-/*     } */
-/*     ++shared()->workers; */
-/*     int32_t id = ++shared()->workers_id; */
-/*     sema_post(&shared()->semaphore); */
+    domain_t lo = get_lo(*env[depth].val);
+    domain_t hi = get_hi(*env[depth].val);
+    domain_t mid = (lo + hi) / 2;
 
-/*     domain_t lo = get_lo(*env[depth].val); */
-/*     domain_t hi = get_hi(*env[depth].val); */
-/*     domain_t mid = (lo + hi) / 2; */
+    // pick up any children that may have finished
+    int status;
+    waitpid(-1, &status, WNOHANG);
 
-/*     // pick up any children that may have finished */
-/*     int status; */
-/*     waitpid(-1, &status, WNOHANG); */
+    pid_t pid = fork();
+    if (pid == -1) {
+      print_error("%s", strerror(errno));
+      exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+      // child gets a new id and searches the upper half
+      _worker_id = id;
+      struct val_t v = (mid+1 == hi) ? VALUE(hi) : INTERVAL(mid+1, hi);
+      bind(env[depth].val, v);
+      _worker_min_depth = depth;
+      // reset stats for child
+      stats_init();
+      depth_min = depth;
+      depth_max = depth;
+    } else {
+      // parent searches the lower half
+      struct val_t v = (lo == mid) ? VALUE(lo) : INTERVAL(lo, mid);
+      bind(env[depth].val, v);
+    }
+  }
+}
 
-/*     pid_t pid = fork(); */
-/*     if (pid == -1) { */
-/*       print_error("%s", strerror(errno)); */
-/*       exit(EXIT_FAILURE); */
-/*     } else if (pid == 0) { */
-/*       // child gets a new id and searches the upper half */
-/*       _worker_id = id; */
-/*       struct val_t v = (mid+1 == hi) ? VALUE(hi) : INTERVAL(mid+1, hi); */
-/*       bind(env[depth].val, v); */
-/*       // reset stats for child */
-/*       stats_init(); */
-/*       depth_min = depth; */
-/*       depth_max = depth; */
-/*     } else { */
-/*       // parent searches the lower half */
-/*       struct val_t v = (lo == mid) ? VALUE(lo) : INTERVAL(lo, mid); */
-/*       bind(env[depth].val, v); */
-/*     } */
-/*     return pid; */
-/*   } */
+void await_children(void) {
+  for (;;) {
+    int status;
+    pid_t pid = wait(&status);
+    if (pid == -1 && errno == ECHILD) {
+      break;
+    }
+  }
+}
 
-/*   return -1; */
-/* } */
+void worker_die(void) {
+  sema_wait(&shared()->semaphore);
+  shared()->workers--;
+  sema_post(&shared()->semaphore);
 
-/* void await_children(void) { */
-/*   for (;;) { */
-/*     int status; */
-/*     pid_t pid = wait(&status); */
-/*     if (pid == -1 && errno == ECHILD) { */
-/*       break; */
-/*     } */
-/*   } */
-/* } */
+  await_children();
 
-/* void worker_die(pid_t pid) { */
-/*   if (pid == 0) { */
-/*     // throttle the reation of new workers */
-/*     if (_workers_max > 1) { */
-/*       usleep(THROTTLE_DELAY); */
-/*     } */
-
-/*     sema_wait(&shared()->semaphore); */
-/*     shared()->workers--; */
-/*     sema_post(&shared()->semaphore); */
-
-/*     await_children(); */
-
-/*     if (calls > 0) { */
-/*       print_stats(stdout); */
-/*     } */
-
-/*     exit(EXIT_SUCCESS); */
-/*   } */
-/* } */
+  if (calls > 0) {
+    print_stats(stdout);
+  }
+}
 
 void swap_env(struct env_t *env, size_t depth1, size_t depth2) {
   if (env[depth1].key != NULL && env[depth2].key != NULL && depth1 != depth2) {
@@ -268,6 +254,7 @@ static bool check_assignment(struct env_t *env, size_t depth) {
 }
 
 static bool check_restart(void) {
+  // check whether search should restart
   if (strategy_restart_frequency() > 0 && objective() == OBJ_ANY) {
     _fail_count++;
 
@@ -281,8 +268,9 @@ static bool check_restart(void) {
   return false;
 }
 
-static void unwind(struct env_t *env, size_t depth, size_t bottom) {
-  for (size_t i = depth; i != (bottom-1); --i) {
+static void unwind(struct env_t *env, size_t depth, size_t stop) {
+  // unwind search steps up to a specified level
+  for (size_t i = depth; i != (stop-1); --i) {
     env[i].step->active = false;
     unbind(env[i].step->bind_depth);
     dealloc(env[i].step->alloc_marker);
@@ -337,6 +325,7 @@ static domain_t step_val(struct env_t *env, size_t depth) {
   return ((i ^ s) & 1) ? hi - (i >> 1) : lo + (i >> 1);
 }
 
+// backtrack by one level
 #define BACKTRACK()                             \
   if (depth != 0) {                             \
     depth--;                                    \
@@ -345,6 +334,7 @@ static domain_t step_val(struct env_t *env, size_t depth) {
     break;                                      \
   }
 
+// restart the search
 #define RESTART()                               \
   {                                             \
     unwind(env, depth, 0);                      \
@@ -352,6 +342,7 @@ static domain_t step_val(struct env_t *env, size_t depth) {
     continue;                                   \
   }
 
+// exit the search
 #define EXIT()                                  \
   {                                             \
     break;                                      \
@@ -363,8 +354,17 @@ void solve(struct env_t *env, struct constr_t *obj, struct constr_t *constr, siz
   env[depth].step->constr = constr;
 
   while (true) {
+    if (depth < _worker_min_depth) {
+      EXIT();
+    }
+
+    // stop searching if a solution is found and we just need any solution
+    if (objective() == OBJ_ANY && shared()->solutions > 0) {
+      EXIT();
+    }
+
+    // check if a better feasible solution is reached
     if (env[depth].key == NULL) {
-      // check if a better feasible solution is reached
       bool updated = update_solution(env, env[depth].step->obj, env[depth].step->constr);
       if (updated) {
         if (objective() == OBJ_ANY) {
@@ -380,17 +380,22 @@ void solve(struct env_t *env, struct constr_t *obj, struct constr_t *constr, siz
     if (!env[depth].step->active) {
       // not active yet, so we can pick a variable
       strategy_pick_var(env, depth);
+      // spawn a worker if possible
+      worker_spawn(env, depth);
       step_activate(env, depth);
     } else {
+      // continue iteration
       step_leave(env, depth);
       step_next(env, depth);
     }
 
+    // check if values for variable are exhausted
     if (!step_check(env, depth)) {
       step_deactivate(env, depth);
       BACKTRACK();
     }
 
+    // try next value
     step_enter(env, depth, step_val(env, depth));
 
     stats_update(depth);
@@ -407,6 +412,7 @@ void solve(struct env_t *env, struct constr_t *obj, struct constr_t *constr, siz
     }
   }
 
-  print_stats(stdout);
+  // wait for children and die
+  worker_die();
 }
 
